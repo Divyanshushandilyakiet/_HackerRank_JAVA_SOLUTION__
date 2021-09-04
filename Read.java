d208786810d0d0cf9780f4620924345f13f6b96b
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-09-04 12:05:19 +0530

    Create Read.java

---
+++ Read.java
@@ -0,0 +1,15 @@
+//Java Stdin and Stdout I
+import java.util.*;
+
+import java.util.Scanner;
+
+public class Read {
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int a =s.nextInt();
+    int b=s.nextInt();
+    int c =s.nextInt();
+     
+    System.out.println(a+"\n"+b+"\n"+c);
+}
+}
