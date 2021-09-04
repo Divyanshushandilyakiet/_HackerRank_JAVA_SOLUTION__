0febae92e68e240569dda9da816a5ee3803b9627
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-09-04 12:06:44 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,21 @@
+//Java If-Else
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a%2==1) {
+            System.out.println("Weird");
+        }
+        else {
+             if (a >= 2 && a <= 5) {
+              System.out.println("Not Weird");
+             } else if (a >= 6 && a <= 20) {
+             System.out.println("Weird");
+             } else {
+              System.out.println("Not Weird");
+             }
+        }
+    }
+}
