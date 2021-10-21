584e3c6db563b8f5c889db0db1e3a1b4ed5a6f3c
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-10-22 04:58:03 +0530

    Create Swap.java

---
+++ INTERVIEW BASED/Swap.java
@@ -0,0 +1,15 @@
+//Swapping of two numbers without third variable
+
+package basicProgram;
+
+public class Swap {
+	public static void main(String[] args) {
+		int x=10;
+		int y=50;
+		System.out.println(x+" "+y);
+		 x=x+y;//x=60
+		 y=x-y;//y=10
+		 x=x-y;//x=50
+		System.out.println(x+" "+y);	
+	} 
+}
