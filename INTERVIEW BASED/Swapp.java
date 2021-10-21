b95d3255addf2dd81b81dc935541eb462375e819
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-10-22 05:06:40 +0530

    Create Swapp.java

---
+++ INTERVIEW BASED/Swapp.java
@@ -0,0 +1,11 @@
+//Swapping of two number with third variable
+public class Swapp {
+public static void main(String[] args) {
+	int x=0,y=1,temp;
+System.out.println(x+" "+y);
+temp=x;
+x=y;
+y=temp;
+System.out.println(x+" "+y);
+}
+}
